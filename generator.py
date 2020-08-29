a08a587599592039975bb819ddd9df4f2144cf6d
Robert Lee <32447910+robertklee@users.noreply.github.com>
2020-08-29 07:11:44 -0700

    fixed data load

--- generator.py
+++ generator.py
@@ -75,8 +75,8 @@ class segmentationGenerator(keras.utils.Sequence):
         imageNames = self.inputs[index*self.batch_size:(index+1)*self.batch_size]
 
         for _, imageNameSet in enumerate(imageNames):
-            img_path = os.path.join(self.img_dir, imageNameSet)
-            seg_path = os.path.join(self.seg_dir, imageNameSet.replace('_', '_road_'))
+            img_path = os.path.join(self.img_dir, imageNameSet[0])
+            seg_path = os.path.join(self.seg_dir, imageNameSet[0].replace('_', '_road_'))
 
             img_orig = cv2.imread(img_path)
             seg_orig = cv2.imread(seg_path)
