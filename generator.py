de3f3ae863f75a16b873fd8a33d1749b43e590f2
Robert Lee <32447910+robertklee@users.noreply.github.com>
2020-08-29 15:26:23 -0700

    since test does not have ground truth, we split train into train and validation

--- generator.py
+++ generator.py
@@ -47,18 +47,22 @@ def preprocess_input(image, randomVals):
     #input_img = preprocess_input(image=input_img, randomVals=randomVals)
 
 class segmentationGenerator(keras.utils.Sequence):
+    img_list = []
+    trainRatio = 0.8 # 80% train, 20% test
     '''Generates data for Keras'''
     '''Framework taken from https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly'''
     '''Provided directories should contain the same number of files all with the same names to their pair image'''
-    def __init__(self, img_dir, seg_dir, batch_size = 64, image_size=(640,192), shuffle=True, agumentations=True):
+    def __init__(self, img_dir, seg_dir, batch_size = 64, image_size=(640,192), shuffle=True, augmentations=True, test=False):
         self.img_dir = img_dir
         self.seg_dir = seg_dir
         self.image_size = image_size
         self.batch_size = batch_size
         self.shuffle = shuffle
-        self.agumentations = agumentations
+        self.augmentations = augmentations
         self.inputs = []
+        self.test = test
         self.initalSetup()
+
 
     def __len__(self):
         '''Denotes the number of batches per epoch'''
@@ -75,24 +79,31 @@ class segmentationGenerator(keras.utils.Sequence):
         imageNames = self.inputs[index*self.batch_size:(index+1)*self.batch_size]
 
         for _, imageNameSet in enumerate(imageNames):
-            img_path = os.path.join(self.img_dir, imageNameSet[0])
-            seg_path = os.path.join(self.seg_dir, imageNameSet[0].replace('_', '_road_'))
+            img_path = os.path.join(self.img_dir, imageNameSet)
+            seg_path = os.path.join(self.seg_dir, imageNameSet.replace('_', '_road_'))
 
             img_orig = cv2.imread(img_path)
             seg_orig = cv2.imread(seg_path)
+            
+            # remove red from output
+            seg_road = seg_orig
+            seg_road[:,:,2] = np.zeros([seg_road.shape[0], seg_road.shape[1]])
 
             if (seg_orig is None):
                 print("Error in seg path: " + seg_path)
                 continue
 
             img = cv2.resize(img_orig, dsize=self.image_size)
-            seg = cv2.resize(seg_orig, dsize=self.image_size)
+            seg = cv2.resize(seg_road, dsize=self.image_size)
             
-            # print(img.shape)
-            # cv2.imshow('test', img)
-            # cv2.waitKey(-1)
+            print(img.shape)
+            cv2.imshow('test', img)
+            cv2.waitKey(-1)
+            print(seg.shape)
+            cv2.imshow('test', seg)
+            cv2.waitKey(-1)
             
-            if self.agumentations:
+            if self.augmentations:
                 randomVals = []
                 for x in range(0,9):
                     randomVals.append(random.random())
@@ -121,28 +132,27 @@ class segmentationGenerator(keras.utils.Sequence):
     
     def initalSetup(self):
         #print("")
-        imgs = os.listdir(self.img_dir)
-        imgs.sort()
+        if (len(segmentationGenerator.img_list) == 0):
+            imgs = os.listdir(self.img_dir)
+            imgs.sort()
 
-        segs = os.listdir(self.seg_dir)
-        segs.sort()
+            systemFiles = '.DS_Store'
+            
+            prefixes = ('.')
+            for word in imgs[:]:
+                if word.startswith(prefixes) or word is systemFiles:
+                    imgs.remove(word)
 
-        systemFiles = '.DS_Store'
+            if self.shuffle:
+                random.shuffle(imgs)
+            
+            segmentationGenerator.img_list = imgs
         
-        prefixes = ('.')
-        for word in imgs[:]:
-            if word.startswith(prefixes):
-                imgs.remove(word)
-            if word is systemFiles:
-                imgs.remove(word)
+        if (self.test):
+            self.inputs = segmentationGenerator.img_list[int(len(segmentationGenerator.img_list) * segmentationGenerator.trainRatio):]
+        else:
+            self.inputs = segmentationGenerator.img_list[0:int(len(segmentationGenerator.img_list) * segmentationGenerator.trainRatio)]
 
-        self.inputs = []
-
-        for imgName in imgs:
-            self.inputs.append([imgName])
-                
-        if self.shuffle:
-            random.shuffle(self.inputs)   
         #self.inputs = self.inputs[0:100]   
         print("")
         print("")
@@ -150,8 +160,10 @@ class segmentationGenerator(keras.utils.Sequence):
 
 
 if __name__ == "__main__":
-    test = segmentationGenerator('data/data_road/training/image_2', 'data/data_road/training/gt_image_2/',   batch_size=8, shuffle=True)
+    train = segmentationGenerator('data/data_road/training/image_2/', 'data/data_road/training/gt_image_2/',   batch_size=8, shuffle=True)
+    test = segmentationGenerator('data/data_road/training/image_2/', 'data/data_road/training/gt_image_2/',   batch_size=8, shuffle=True, test=True)
 
+    train.__getitem__(1)
     test.__getitem__(1)
 
     print('Data generator test success.')
