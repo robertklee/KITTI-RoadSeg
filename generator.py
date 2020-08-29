4a4c0465bef838811de5dfbbadc42358165b87f9
Robert Lee <32447910+robertklee@users.noreply.github.com>
2020-08-29 06:03:31 -0700

    push for colab testing

---
+++ generator.py
@@ -0,0 +1,147 @@
+import keras
+import os
+import random
+import numpy as np
+import cv2
+
+# TODO use imgaug for more robust image augmentation
+def preprocess_input(image, randomVals):
+    '''
+    performs data augmentations listed below each with chance == 50%
+    - Horiontal flip
+    - Random brightness +- 0.2
+    - Random contrast +- 0.2
+    - Random saturation +- 0.2
+    - Hue Jitter +- 0.1
+
+    all random values provided in range (0,1)
+    '''
+    if randomVals[0] > 0.5:
+        # flip image horizontally
+        #image = np.flip(image, 1)
+        None
+    if randomVals[1] > 0.5:
+        # increase/ decrease contrast
+        image = np.uint8(np.clip(image * (0.8 + randomVals[2]/2.5), a_min=0, a_max=255))
+    # Convert image to HSV for some transformations
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV).astype(np.int32)
+    if randomVals[3] > 0.5:
+        # change brightness of image
+        hsv_image[:,:,2] += int(((randomVals[4]/2.5 )- 0.2 ) * 255.) 
+        hsv_image[:,:,2] = np.clip(hsv_image[:,:,2], a_min =0, a_max = 255) 
+    if randomVals[5] > 0.5:
+        # change staturation
+        hsv_image[:,:,1] += int(((randomVals[6]/2.5 )- 0.2 ) * 255.)
+        hsv_image[:,:,1] = np.clip(hsv_image[:,:,1], a_min = 0, a_max = 255) 
+    if randomVals[7] > 0.5:
+        # change Hue
+        hsv_image[:,:,0] += int(((randomVals[8]/2.5 )- 0.2 ) * 179.)
+        hsv_image[:,:,0] = np.clip(hsv_image[:,:,0], a_min = 0, a_max = 179) 
+    # Convert image back from HSV
+    image = cv2.cvtColor(np.uint8(hsv_image), cv2.COLOR_HSV2BGR)
+    return image
+
+
+    #for x in range(0,9):
+    #    randomVals.append(random.random())
+    #input_img = preprocess_input(image=input_img, randomVals=randomVals)
+
+class segmentationGenerator(keras.utils.Sequence):
+    '''Generates data for Keras'''
+    '''Framework taken from https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly'''
+    '''Provided directories should contain the same number of files all with the same names to their pair image'''
+    def __init__(self, img_dir, seg_dir, batch_size = 64, image_size=(640,192), shuffle=True, agumentations=True):
+        self.img_dir = img_dir
+        self.seg_dir = seg_dir
+        self.image_size = image_size
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.agumentations = agumentations
+        self.inputs = []
+        self.initalSetup()
+
+    def __len__(self):
+        '''Denotes the number of batches per epoch'''
+        return int(np.floor(len(self.inputs) / self.batch_size))
+
+
+    def __getitem__(self, index):
+        '''Generate one batch of data'''
+        outX  =  np.empty((self.batch_size,  *self.image_size, 3))
+        outY  =  np.empty((self.batch_size,  *self.image_size, 3))
+        outY_0 = np.empty((self.batch_size, *self.image_size, 3))
+        outY_1 = np.empty((self.batch_size, *self.image_size, 3))
+
+        imageNames = self.inputs[index*self.batch_size:(index+1)*self.batch_size]
+
+        for _, imageNameSet in enumerate(imageNames):
+            img_orig = cv2.imread(os.path.join(self.img_dir, imageNameSet[0]))
+            seg_orig = cv2.imread(os.path.join(self.seg_dir, imageNameSet[0].replace('_', '_road_')))
+
+            img = cv2.resize(img_orig, dsize=self.image_size)
+            seg = cv2.resize(seg_orig, dsize=self.image_size)
+            
+            # print(img.shape)
+            # cv2.imshow('test', img)
+            # cv2.waitKey(-1)
+            
+            if self.agumentations:
+                randomVals = []
+                for x in range(0,9):
+                    randomVals.append(random.random())
+                img_augmented = preprocess_input(image=img, randomVals=randomVals)
+            else:
+                img_augmented = img
+
+            outX[_]   =  np.transpose(img_augmented, axes=[1,0,2])
+            # outY_0[_] =  np.transpose(img,           axes=[1,0,2])
+            # outY_1[_] =  np.transpose(seg,           axes=[1,0,2])
+            outY[_] =  np.transpose(seg,           axes=[1,0,2])
+
+            #test_out = outX[_].astype('uint8')#np.transpose(left_augmented,     axes=[1,0,2])
+            #cv2.imshow('test', test_out)
+            #cv2.waitKey(-1)
+
+            # outY = np.concatenate([outY_0,outY_1], axis=3)
+
+        return outX, outY #[outY_0, outY_1, outY_2, outY_3]
+                        
+
+    def on_epoch_end(self):
+        ''' Shuffle the data if that is required'''
+        if self.shuffle:
+            random.shuffle(self.inputs)   
+    
+    def initalSetup(self):
+        #print("")
+        imgs = os.listdir(self.img_dir)
+        imgs.sort()
+
+        systemFiles = '.DS_Store'
+        
+        prefixes = ('.')
+        for word in imgs[:]:
+            if word.startswith(prefixes):
+                imgs.remove(word)
+            if word is systemFiles:
+                imgs.remove(word)
+
+        self.inputs = []
+
+        for imgName in imgs:
+            self.inputs.append([imgName])
+                
+        if self.shuffle:
+            random.shuffle(self.inputs)   
+        #self.inputs = self.inputs[0:100]   
+        print("")
+        print("")
+
+
+
+if __name__ == "__main__":
+    test = segmentationGenerator('data/data_road/training/image_2', 'data/data_road/training/gt_image_2/',   batch_size=8, shuffle=True)
+
+    test.__getitem__(1)
+
+    print('Data generator test success.')
---
+++ resnet_model.py
@@ -0,0 +1,211 @@
+import keras
+from keras.layers import Conv2D, BatchNormalization, MaxPool2D, Flatten, AveragePooling2D, concatenate, Add, Activation, Input, UpSampling2D, Conv2DTranspose
+
+from keras import Model
+from keras.optimizers import Adam
+
+from resnet_def import ResNet50, ResNet18
+import constants
+
+'''
+
+ref:
+https://medium.com/@14prakash/understanding-and-implementing-architectures-of-resnet-and-resnext-for-state-of-the-art-image-cf51669e1624    
+https://keras.io/applications/#resnet
+https://towardsdatascience.com/understanding-and-visualizing-resnets-442284831be8
+'''
+
+def generateResNetEncoderLayers(inputLayer, resnetType=18):
+    '''
+    takes an input layer of type Input from keras   
+
+    returns the output layer of a resent of the specifified type
+    input layer must be of form: (batches, even#, even#, 3*numImages)
+    '''
+    assert resnetType in [50,18]
+    if resnetType == 50:
+        x = ResNetLayerInitialStage(inputLayer)
+
+        x = ResNetLayerLaterState_50(x, 64, [64,64,256],1,1)
+        x = ResNetLayerLaterState_50(x, 256, [64,64,256],1,2)
+        x = ResNetLayerLaterState_50(x, 256, [64,64,256],1,3)
+        
+        x1 = ResNetLayerLaterState_50(x, 256, [128,128,512],2,4)
+        x1 = ResNetLayerLaterState_50(x1, 512, [128,128,512],1,5)
+        x1 = ResNetLayerLaterState_50(x1, 512, [128,128,512],1,6)
+        x1 = ResNetLayerLaterState_50(x1, 512, [128,128,512],1,7)
+
+        x2 = ResNetLayerLaterState_50(x1, 512, [256,256,1024],2,8)
+        x2 = ResNetLayerLaterState_50(x2, 1024, [256,256,1024],1,9)
+        x2 = ResNetLayerLaterState_50(x2, 1024, [256,256,1024],1,10)
+        x2 = ResNetLayerLaterState_50(x2, 1024, [256,256,1024],1,11)
+        x2 = ResNetLayerLaterState_50(x2, 1024, [256,256,1024],1,12)
+        x2 = ResNetLayerLaterState_50(x2, 1024, [256,256,1024],1,13)
+
+        x3 = ResNetLayerLaterState_50(x2, 1024, [512,512,2048],2,14)
+        x3 = ResNetLayerLaterState_50(x3, 2048, [512,512,2048],1,15)
+        x3 = ResNetLayerLaterState_50(x3, 2048, [512,512,2048],1,16)
+
+        x3 = ResNetOuputStage(x3)
+        return x3, x2, x1, x
+
+    else: # is resnet 18
+        x = ResNetLayerInitialStage(inputLayer)
+        
+        x = ResNetLayerLaterState_18(x, 64, [64,64],1,1)
+        x = ResNetLayerLaterState_18(x, 64, [64,64],1,2)
+
+        x1 = ResNetLayerLaterState_18(x, 64, [128,128],2,3)
+        x1 = ResNetLayerLaterState_18(x1, 128, [128,128],1,4)
+
+        x2 = ResNetLayerLaterState_18(x1, 128, [256,256],2,6)
+        x2 = ResNetLayerLaterState_18(x2, 256, [256,256],1,7)
+        
+        x3 = ResNetLayerLaterState_18(x2, 256, [512,512],2,8)
+        x3 = ResNetLayerLaterState_18(x3, 512, [512,512],1,9)
+
+        x3 = ResNetOuputStage(x3)
+        return x3, x2, x1, x
+
+def ResNetLayerInitialStage(inputLayer):
+    x = Conv2D(filters=64,kernel_size=7,strides=2,data_format='channels_last',activation='relu',padding='same', name="InitialConv")(inputLayer)
+    x = BatchNormalization(axis=3)(x)
+    x = MaxPool2D(pool_size=(3,3),strides=2, data_format='channels_last',padding='same', name="InitalPool")(x)
+    return x
+
+def ResNetLayerLaterState_50(inputLayer, inputChannels, channels, poolingStride, resNetBlockID):
+    '''
+    3 convolutional blocks
+
+    1x1, channels[0], relu
+    3x3, channels[1], relu
+    1x1, channels[2], linear
+    add input and output
+    relu
+    '''
+
+    assert len(channels) == 3
+
+    x = Conv2D(channels[0], kernel_size=poolingStride, strides=poolingStride,data_format='channels_last',activation='relu',padding='same', name="Conv1_" + str(inputChannels) + "__" + str(resNetBlockID))(inputLayer)
+    x = BatchNormalization(axis=3)(x)
+    x = Conv2D(channels[1], kernel_size=3,strides=1,data_format='channels_last',activation='relu',padding='same', name="Conv2_" + str(inputChannels) + "__" + str(resNetBlockID))(x)
+    x = BatchNormalization(axis=3)(x)
+    x = Conv2D(channels[2], kernel_size=1,strides=1,data_format='channels_last',activation='linear',padding='same', name="Conv3_" + str(inputChannels) + "__" + str(resNetBlockID))(x)
+    x = BatchNormalization(axis=3)(x)
+
+    if inputChannels != channels[2]:
+        # this could be zero padding but instread were doing 1x1 stride 1 convolution to make the shapes the same, both are technically from paper acceptable
+        inputLayer = Conv2D(channels[2], kernel_size=1, strides=1,data_format='channels_last',activation='linear',padding='same', name="ConvSkip_" + str(inputChannels) + "__" + str(resNetBlockID))(inputLayer)
+        if poolingStride != 1:
+            inputLayer = MaxPool2D(pool_size=2)(inputLayer)
+
+    x = Add()([x,inputLayer])
+    x = Activation('relu')(x)
+    return x
+
+def ResNetLayerLaterState_18(inputLayer, inputChannels, channels, poolingStride, resNetBlockID):
+    '''
+    two convolutional blocks 
+    3x3, channels[0], relu
+    3x3, channels[0], linear
+    add input and output
+    relu
+    '''
+
+    assert len(channels) == 2
+
+    x = Conv2D(channels[0], kernel_size=3,strides=poolingStride,data_format='channels_last',activation='relu',padding='same', name="Conv1_" + str(inputChannels) + "__" + str(resNetBlockID))(inputLayer)
+    x = BatchNormalization(axis=3)(x)
+    x = Conv2D(channels[1], kernel_size=3,strides=1,data_format='channels_last',activation='linear',padding='same', name="Conv2_" + str(inputChannels) + "__" + str(resNetBlockID))(x)
+    x = BatchNormalization(axis=3)(x)
+
+    if inputChannels != channels[1]:
+        # this should be zero padding but just simple one conv for now untill fixed
+        inputLayer = Conv2D(channels[1], kernel_size=1, strides=1,data_format='channels_last',activation='linear',padding='same', name="ConvSkip_" + str(inputChannels) + "__" + str(resNetBlockID))(inputLayer)
+        if poolingStride != 1:
+            inputLayer = MaxPool2D(pool_size=2,padding='same', name="Pool_" + str(inputChannels) + "__" + str(resNetBlockID))(inputLayer)
+
+    x = Add()([x,inputLayer])
+    x = Activation('relu')(x)
+
+    return x
+
+def ResNetOuputStage(inputLayer, pools=1000):
+    output = AveragePooling2D(pool_size=2,strides=1,padding='same')(inputLayer)
+    return output
+
+def buildDecoder(inputLayer, scale_1, scale_2, scale_3, outputChannels=1):
+    pass
+
+    x = Conv2D(1024, kernel_size=3, strides=1, data_format='channels_last',padding='same', name="DecoderConv_Block_1_1", activation='relu')(inputLayer)
+    x = Conv2D(1024, kernel_size=3, strides=1, data_format='channels_last',padding='same', name="DecoderConv_Block_1_2", activation='relu')(x)
+
+    x = Conv2DTranspose(512, (2, 2), strides=(2, 2), data_format='channels_last', name="ConvTranspose1")(x)
+
+    x = concatenate([x,scale_1],axis=3)
+
+    x = Conv2D(512, kernel_size=3, strides=1, data_format='channels_last', padding='same', name="DecoderConv_Block_2_1", activation='relu')(x)
+    x = Conv2D(512, kernel_size=3, strides=1, data_format='channels_last', padding='same', name="DecoderConv_Block_2_2", activation='relu')(x)
+
+    x = Conv2DTranspose(256, (2, 2), strides=(2, 2), data_format='channels_last', name="ConvTranspose2")(x)
+
+    x = concatenate([x,scale_2],axis=3)
+
+    x = Conv2D(256, kernel_size=3, strides=1, data_format='channels_last', padding='same', name="DecoderConv_Block_3_1", activation='relu')(x)
+    x = Conv2D(256, kernel_size=3, strides=1, data_format='channels_last', padding='same', name="DecoderConv_Block_3_2", activation='relu')(x)
+
+    scale_3_out = Conv2D(32, kernel_size=3, strides=1, data_format='channels_last' ,padding='same', name="EndingConvBlock_Scale3")(x)
+    scale_3_out = Conv2D(outputChannels, kernel_size=3, strides=1, data_format='channels_last' ,padding='same', name="OutputConvBlock_Scale3")(scale_3_out)
+    scale_3_out = UpSampling2D(data_format='channels_last', name="upSampleScale3Out", size=(8,8), interpolation='bilinear')(scale_3_out)
+
+    x = Conv2DTranspose(128, (2, 2), strides=(2, 2), data_format='channels_last', name="ConvTranspose3")(x)
+
+    x = concatenate([x,scale_3],axis=3)
+
+    x = Conv2D(128, kernel_size=3, strides=1, data_format='channels_last', padding='same', name="DecoderConv_Block_4_1", activation='relu')(x)
+    x = Conv2D(128, kernel_size=3, strides=1, data_format='channels_last', padding='same', name="DecoderConv_Block_4_2", activation='relu')(x)
+
+    scale_2_out = Conv2D(32, kernel_size=3, strides=1, data_format='channels_last' ,padding='same', name="EndingConvBlock_Scale2")(x)
+    scale_2_out = Conv2D(outputChannels, kernel_size=3, strides=1, data_format='channels_last' ,padding='same', name="OutputConvBlock_Scale2")(scale_2_out)
+    scale_2_out = UpSampling2D(data_format='channels_last', name="upSampleScale2Out", size=(4,4), interpolation='bilinear')(scale_2_out)
+
+    x = Conv2DTranspose(64, (2, 2), strides=(2, 2), data_format='channels_last', name="ConvTranspose4")(x)
+
+    x = Conv2D(64, kernel_size=3, strides=1, data_format='channels_last', padding='same', name="DecoderConv_Block_5_1", activation='relu')(x)
+    x = Conv2D(64, kernel_size=3, strides=1, data_format='channels_last', padding='same', name="DecoderConv_Block_5_2", activation='relu')(x)
+    
+    scale_1_out = Conv2D(64, kernel_size=3, strides=1, data_format='channels_last' ,padding='same', name="EndingConvBlock_Scale1")(x)
+    scale_1_out = Conv2D(outputChannels, kernel_size=3, strides=1, data_format='channels_last' ,padding='same', name="OutputConvBlock_Scale1")(scale_1_out)
+    scale_1_out = UpSampling2D(data_format='channels_last', name="upSampleScale1Out", size=(2,2), interpolation='bilinear')(scale_1_out)
+    
+    x = Conv2DTranspose(64, (2, 2), strides=(2, 2), data_format='channels_last', name="ConvTranspose5")(x)
+
+    x = Conv2D(64, kernel_size=3, strides=1, data_format='channels_last' ,padding='same', name="EndingConvBlock1", activation='relu')(x)
+    x = Conv2D(64, kernel_size=3, strides=1, data_format='channels_last' ,padding='same', name="EndingConvBlock2", activation='relu')(x)
+    x = Conv2D(outputChannels, kernel_size=3, strides=1, data_format='channels_last' ,padding='same', name="OutputConvBlock", activation='sigmoid')(x)
+    
+    return concatenate([x, scale_1_out, scale_2_out], axis=3), scale_1_out, scale_2_out
+
+
+def create_Model(input_shape=constants.input_shape, encoder_type=50):
+    if encoder_type == 50:
+        inputLayer, outputLayer, scaleLayers = ResNet50(input_shape=constants.input_shape,include_top=False, create_encoder=True)
+        networkOuput, scale_1_out, scale_2_out = buildDecoder(outputLayer, scaleLayers[2], scaleLayers[1], scaleLayers[0], 1)
+        model = Model(inputs=[inputLayer], outputs=[networkOuput])#, scale_1_out, scale_2_out, scale_3_out])
+        model.load_weights(constants.resnet_50_model_path, by_name=True)
+        model.summary()
+    if encoder_type == 18:
+        inputLayer, outputLayer, scaleLayers = ResNet18(input_shape=constants.input_shape,include_top=False, create_encoder=True)
+        networkOuput, scale_1_out, scale_2_out = buildDecoder(outputLayer, scaleLayers[2], scaleLayers[1], scaleLayers[0], 1)
+        model = Model(inputs=[inputLayer], outputs=[networkOuput])#, scale_1_out, scale_2_out, scale_3_out])
+        model.load_weights(constants.resnet_18_model_path, by_name=True)
+        model.summary()
+    return model
+
+if __name__ == "__main__":    
+
+    print("Testing creating models")
+    model = create_Model(input_shape=constants.input_shape, encoder_type=18)
+    print("Done creating ResNet18 backbone model")
+    model = create_Model(input_shape=constants.input_shape_full_size, encoder_type=50)
+    print("Done creating ResNet50 backbone model")
