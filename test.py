0bae174e57b9a6228645db2f067b414917e4d1ce
Robert Lee <32447910+robertklee@users.noreply.github.com>
2020-08-30 14:00:56 -0700

    add support for new unet decoder and new output

--- test.py
+++ test.py
@@ -17,6 +17,7 @@ import constants
 import argparse
 from helpers import make_overlay
 import re
+import matplotlib.pyplot as plt
 
 
 '''
@@ -27,13 +28,15 @@ overlay image
 examine model output since the values are very large
 quick way to refer to a specific epoch checkpoint file
 evaluation metrics for generated output
+
+linear histogram equilization
 '''
 
 argparser = argparse.ArgumentParser(description='Testing')
 
-model_epoch_number = 14#16
+model_epoch_number = 20#16
 resnet_type = 18
-session_id = 'KITTI_Road_2020_08_20_batchsize_12'
+session_id = 'KITTI_Road_UNet_Sun Aug 30 19_52_21 2020_batchsize_25_resnet_18'
 batchSize = 1
 model_epoch_base = '_weights_epoch'
 output_img_base_dir = 'output'
@@ -76,7 +79,7 @@ model_path = os.path.join(model_base_dir, args.session)
 def get_model_name_from_epoch(src, epoch):
     models = os.listdir(src)
 
-    pattern = re.compile(model_epoch_base + "[0]*" + str(args.epoch))
+    pattern = re.compile(model_epoch_base + "[a-zA-Z_]*[0]*" + str(args.epoch))
 
     for modelName in models:
         if pattern.search(modelName):
@@ -100,6 +103,16 @@ model = create_Model(input_shape=(640,192,3), encoder_type=args.resnet)
 
 model.compile(optimizer=Adam(lr=1e-3),loss=loss, metrics=['accuracy'])
 
+def modelPredictWrapper(model, inputImg):
+    modelOutput = model.predict(np.expand_dims(inputImg,0))# * 640 * 0.3
+
+    if constants.use_unet:
+        # change from (1, 640, 192, 2) to (1, 640, 192, 3)
+        zeros = np.zeros([modelOutput.shape[0], modelOutput.shape[1], modelOutput.shape[2], 1])
+        modelOutput = np.concatenate((modelOutput, zeros), axis=3)
+    
+    return modelOutput
+
 def evaluateModel(model,batchSize, visualize):
     val_generator  = segmentationGenerator('data/data_road/training/image_2','data/data_road/training/gt_image_2', batch_size=args.batch, shuffle=False, augmentations=False)
     # scores = model.evaluate_generator(val_generator, verbose=1)
@@ -118,17 +131,30 @@ def evaluateModel(model,batchSize, visualize):
     rawImage = cv2.resize(inputImg, (640,192))
     inputImgOrig  = cv2.resize(cv2.imread('data/data_road/testing/image_2/' + imageName), (640,192))
     inputImg  = np.transpose(inputImgOrig.astype('float32'), axes=[1,0,2])
-    output = model.predict(np.expand_dims(inputImg,0))# * 640 * 0.3
+    output = modelPredictWrapper(model, inputImg)
     def displayOutput(output, dim=0):
         output = np.squeeze(output)
         outputTransformed = np.transpose(  output,    axes=[1,0,2])
-        outputTransformed = outputTransformed - np.mean(outputTransformed)
-        outputTransformed = np.clip(outputTransformed, (np.mean(outputTransformed) - np.std(outputTransformed)), (np.mean(outputTransformed) + np.std(outputTransformed)))
-        outputTransformed = outputTransformed - np.min(outputTransformed)
+        # outputTransformed = outputTransformed - np.mean(outputTransformed)
+        # outputTransformed = np.clip(outputTransformed, (np.mean(outputTransformed) - 2*np.std(outputTransformed)), (np.mean(outputTransformed) + 2*np.std(outputTransformed)))
+        # outputTransformed = outputTransformed - np.min(outputTransformed)
         outputTransformed = np.clip(outputTransformed / np.max(outputTransformed) * 255, 0, 255).astype('uint8')
         return outputTransformed
 
+    def generateHist(output, name, dim=0):
+        outputSqueezed = output.squeeze()
+
+        outputChannelSingle = outputSqueezed[:,:,dim]
+        _ = plt.hist(outputChannelSingle, bins='auto')  # arguments are passed to np.histogram
+        plt.title("Histogram with 'auto' bins")
+
+        plt.show()
+        plt.savefig(name)
+        plt.close()
+
     outputDisplay = displayOutput(output, 2)
+
+
 
     overlayedImage = cv2.addWeighted(inputImgOrig, 0.8, outputDisplay, 0.2, 0)
 
@@ -151,7 +177,7 @@ def evaluateModel(model,batchSize, visualize):
             continue
         inputImgOrig    = cv2.resize(cv2.imread('data/data_road/testing/image_2/' + filename), (640,192))
         inputImg    = np.transpose(inputImgOrig.astype('float32'),      axes=[1,0,2])
-        output = model.predict(np.expand_dims(inputImg,0)) #* 640 * 0.3
+        output = modelPredictWrapper(model, inputImg)
         count += 1
         outputTransformed = displayOutput(output)
 
@@ -160,13 +186,17 @@ def evaluateModel(model,batchSize, visualize):
         cv2.imwrite(os.path.join(output_img_path, filename),  outputTransformed )
         cv2.imwrite(os.path.join(output_img_path, "overlay_" + filename),  overlayedImage )
 
+        # generateHist(output, os.path.join(output_img_path, "overlay_" + filename[:-4] + "_hist_0" + filename[-4:]),0)
+        # generateHist(output, os.path.join(output_img_path, "overlay_" + filename[:-4] + "_hist_1" + filename[-4:]),1)
+        # generateHist(output, os.path.join(output_img_path, "overlay_" + filename[:-4] + "_hist_2" + filename[-4:]),2)
+
         print(count, " of ", len(imgs) , end='\r')
     print("Mean ARD: ", ARD / count)
     print("Mean SQR: ", SQR / count)
     print("")
 
 
-print("Testing model trained on MacBook")
+print("Testing model: {}".format(model_name))
 if not os.path.exists(output_img_path):
     os.makedirs(output_img_path)
 
