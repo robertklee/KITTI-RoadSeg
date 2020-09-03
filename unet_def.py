6f402540a99744bd0c7b1f194eb2b71b12ebc6a7
Robert Lee <32447910+robertklee@users.noreply.github.com>
2020-09-02 23:50:56 -0700

    comment code

--- unet_def.py
+++ unet_def.py
@@ -7,46 +7,47 @@ from keras.layers import (Activation, Add, AveragePooling2D,
 import keras.backend as backend
 import keras.utils as keras_utils
 
-# def get_segmentation_model(input, output):
+'''
+def get_segmentation_model(input, output):
 
-#     img_input = input
-#     o = output
+    img_input = input
+    o = output
 
-#     o_shape = Model(img_input, o).output_shape
-#     i_shape = Model(img_input, o).input_shape
+    o_shape = Model(img_input, o).output_shape
+    i_shape = Model(img_input, o).input_shape
 
-#     if IMAGE_ORDERING == 'channels_first':
-#         output_height = o_shape[2]
-#         output_width = o_shape[3]
-#         input_height = i_shape[2]
-#         input_width = i_shape[3]
-#         n_classes = o_shape[1]
-#         o = (Reshape((-1, output_height*output_width)))(o)
-#         o = (Permute((2, 1)))(o)
-#     elif IMAGE_ORDERING == 'channels_last':
-#         output_height = o_shape[1]
-#         output_width = o_shape[2]
-#         input_height = i_shape[1]
-#         input_width = i_shape[2]
-#         n_classes = o_shape[3]
-#         o = (Reshape((output_height*output_width, -1)))(o)
+    if IMAGE_ORDERING == 'channels_first':
+        output_height = o_shape[2]
+        output_width = o_shape[3]
+        input_height = i_shape[2]
+        input_width = i_shape[3]
+        n_classes = o_shape[1]
+        o = (Reshape((-1, output_height*output_width)))(o)
+        o = (Permute((2, 1)))(o)
+    elif IMAGE_ORDERING == 'channels_last':
+        output_height = o_shape[1]
+        output_width = o_shape[2]
+        input_height = i_shape[1]
+        input_width = i_shape[2]
+        n_classes = o_shape[3]
+        o = (Reshape((output_height*output_width, -1)))(o)
 
-#     o = (Activation('softmax'))(o)
-#     model = Model(img_input, o)
-#     model.output_width = output_width
-#     model.output_height = output_height
-#     model.n_classes = n_classes
-#     model.input_height = input_height
-#     model.input_width = input_width
-#     model.model_name = ""
+    o = (Activation('softmax'))(o)
+    model = Model(img_input, o)
+    model.output_width = output_width
+    model.output_height = output_height
+    model.n_classes = n_classes
+    model.input_height = input_height
+    model.input_width = input_width
+    model.model_name = ""
 
-#     model.train = MethodType(train, model)
-#     model.predict_segmentation = MethodType(predict, model)
-#     model.predict_multiple = MethodType(predict_multiple, model)
-#     model.evaluate_segmentation = MethodType(evaluate, model)
+    model.train = MethodType(train, model)
+    model.predict_segmentation = MethodType(predict, model)
+    model.predict_multiple = MethodType(predict_multiple, model)
+    model.evaluate_segmentation = MethodType(evaluate, model)
 
-#     return model
-
+    return model
+'''
 # Decoder for UNet is adapted from keras-segmentation
 # https://github.com/divamgupta/image-segmentation-keras/blob/master/keras_segmentation/models/unet.py
 def UNet(f4, f3, f2, f1, output_height, output_width, l1_skip_conn=True, n_classes=2):
